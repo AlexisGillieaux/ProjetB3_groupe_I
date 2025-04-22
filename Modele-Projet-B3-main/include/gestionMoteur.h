8e1ce4a0f95eca6640af93a56eafd4993f320354
AlexisGillieaux <etu55085@student.henallux.be>
2025-04-22 13:50:58 +0200

    Initialize motor control and add matrix header implementation

--- Modele-Projet-B3-main/include/gestionMoteur.h
+++ Modele-Projet-B3-main/include/gestionMoteur.h
@@ -7,8 +7,7 @@
 // Declaration des variables globales
 double speed1=80; // Vitesse du moteur avant
 double speed2=40; // Vitesse du moteur reculer
-L298NX2 moteur(PIN_AIN1, PIN_AIN2, PIN_BIN1, PIN_BIN2); // Initialisation du moteur avec les broches définies dans config.h
-// L298NX2 moteur(PIN_AIN1, PIN_AIN2, PIN_BIN1, PIN_BIN2); 
+
 
 // Déclaration des fonctions
 void Avancer();;
