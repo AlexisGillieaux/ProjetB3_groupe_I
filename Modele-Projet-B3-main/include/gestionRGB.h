815e26923f2a0989194d401a31bbe21b36b923d9
MpoyiHena <etu57430@henallux.be>
2025-04-22 16:10:55 +0200

    Add gestionRGB class and implementation for RGB color detection

---
+++ Modele-Projet-B3-main/include/gestionRGB.h
@@ -0,0 +1,24 @@
+#include <config.h>
+#ifndef GESTIONRGB_H
+#define GESTIONRGB_H
+
+#include <stdint.h> // Pour les types uint16_t
+
+class GestionRGB {
+public:
+    // Constructeur par défaut
+    GestionRGB();
+
+    // Constructeur avec paramètres pour initialiser les valeurs RGB
+    GestionRGB(int red, int green, int blue);
+
+    // Méthode pour détecter et renvoyer la couleur détectée
+    char* detectColor() const;
+
+private:
+    int redValue;
+    int greenValue;
+    int blueValue;
+};
+
+#endif // GESTIONRGB_H
---
+++ Modele-Projet-B3-main/src/gestionRGB.cpp
@@ -0,0 +1,40 @@
+#include "gestionRGB.h"
+#include <string.h> // Pour utiliser strcpy
+
+
+// Constructeur par défaut
+void setup() 
+{
+  Serial.begin(115200);
+  Serial.println("Color View Test!");
+
+  while(!tcs.begin())
+  {
+    Serial.println("No TCS34725 found ... check your connections");
+    delay(1000);
+  }
+}
+
+void runOnce() {
+  uint16_t clear, red, green, blue;
+  tcs.getRGBC(&red, &green, &blue, &clear);
+  // turn off LED
+  tcs.lock();  
+  Serial.print("C:\t"); Serial.print(clear);
+  Serial.print("\tR:\t"); Serial.print(red);
+  Serial.print("\tG:\t"); Serial.print(green);
+  Serial.print("\tB:\t"); Serial.print(blue);
+  Serial.println("\t");
+  
+  // Figure out some basic hex code for visualization
+  uint32_t sum = clear;
+  float r, g, b;
+  r = red; r /= sum;
+  g = green; g /= sum;
+  b = blue; b /= sum;
+  r *= 256; g *= 256; b *= 256;
+  Serial.print("\t");
+  Serial.print((int)r, HEX); Serial.print((int)g, HEX); Serial.print((int)b, HEX);
+  Serial.println();
+  
+}
