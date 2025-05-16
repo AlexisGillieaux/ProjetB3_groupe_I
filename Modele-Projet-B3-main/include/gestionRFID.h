7cfb287446f60d150ec8997e5d21a2bd261c3964
AlexisGillieaux <etu55085@student.henallux.be>
2025-05-16 14:57:21 +0200

    Remove unused A* and Dijkstra algorithm implementations; add motor pin definitions for Groupe J

--- Modele-Projet-B3-main/include/gestionRFID.h
+++ Modele-Projet-B3-main/include/gestionRFID.h
@@ -1,12 +1,16 @@
 #ifndef RFID_H
 #define RFID_H
- 
+
 #include "config.h"
- 
+
 // Déclaration des fonctions
 void rfid_init();
 void rfidddd();
 bool rfid1Block(byte block );
- 
- 
+int** processMatrix(int* matriceNonDecode);
+int** matrixToNodes(int** matrix);
+int* dijkstra(int** nodes, int start[]);
+int* path (int* prev,int goal[], int start[]);
+int* antecedant(int* prev, int goal[], int start[]);
+
 #endif
--- Modele-Projet-B3-main/src/main.cpp
+++ Modele-Projet-B3-main/src/main.cpp
@@ -9,6 +9,7 @@
 #include <IRremote.hpp>
 #include "IR.h"
 #include "gestionRFID.h"
+#include "MoteurGroupeJ.h"
 
 
 // Définition des variables globales
@@ -26,7 +27,6 @@ L298NX2 moteur(PIN_AIN1, PIN_AIN2, PIN_BIN1, PIN_BIN2);
 int* matriceNonDecode = (int*)malloc(13 * 13 * sizeof(int)); 
 char matriceNonDecodeChar[] = "1111111111111112010041000111101011101011010100000101101011111110110100000000011011111111101100000001003110101111101311010100000121101110111110110000000000011111111111111";
 int** nodes = (int**)malloc(36*sizeof(int*));
-
 
 
 
@@ -50,6 +50,12 @@ void setup() {
   
   // Configuration des broches
   pinMode(PIN_LED, OUTPUT);
+  pinMode(PIN_MOTEURJ_A1, OUTPUT);
+  pinMode(PIN_MOTEURJ_A2, OUTPUT);
+  pinMode(PIN_MOTEURJ_ENA, OUTPUT);
+  pinMode(PIN_MOTEURJ_B1, OUTPUT);
+  pinMode(PIN_MOTEURJ_B2, OUTPUT);
+  pinMode(PIN_MOTEURJ_ENB, OUTPUT);
   // Pas besoin de configurer PIN_BOUTON car la librairie Button s'en charge
   
   // Configuration des transitions
@@ -68,7 +74,17 @@ void loop() {
   // rfidddd();
   // Mettre à jour la machine d'état
   //machine.run();
+  // Serial.println("testMoteurGroupeJ");
+  // testMoteurAStopGroupeJ();
+  // testMoteurBStopGroupeJ();
+  // delay(1000);
+  // testMoteurAAvancerGroupeJ();
+  // testMoteurBAvancerGroupeJ();
+  // delay(1000);
   if (i ==0){
+    // testMoteurAAvancerGroupeJ();
+    // testMoteurBAvancerGroupeJ();
+    // delay(1000);
     Serial.println("Matrice non traitée :");
     for (int j = 0; j < 169; j++)
     {
@@ -113,7 +129,7 @@ void loop() {
     int startNode[7] = {0, 0, 0, 0, 0, 0, 0}; // Initialiser avec des valeurs par défaut
     int goalNode[7] =  {0, 0, 0, 0, 0, 0, 0};  // Initialiser avec des valeurs par défaut
 
-    // Trouver les coordonnées des noeuds de départ et d'arrivée
+  //   // Trouver les coordonnées des noeuds de départ et d'arrivée
     for (int j = 0; j < 36; j++) {
       if (nodes[j][3] == 1) { // Noeud de départ
         startNode[0] = nodes[j][0];
@@ -123,22 +139,22 @@ void loop() {
         startNode[4] = nodes[j][4];
         startNode[5] = nodes[j][5];
         startNode[6] = nodes[j][6];
-        // Serial.print("Noeud de depart ");
-        // Serial.print(nodes[j][0]);
-        // Serial.print(" : ");
-        // Serial.print(nodes[j][1]);
-        // Serial.print(" ");
-        // Serial.print(nodes[j][2]);
-        // Serial.print(" ");
-        // Serial.print(nodes[j][3]);
-        // Serial.print(" ");
-        // Serial.print(nodes[j][4]);
-        // Serial.print(" ");
-        // Serial.print(nodes[j][5]);
-        // Serial.print(" ");
-        // Serial.print(nodes[j][6]);
-        // Serial.print(" ");
-        // Serial.println(nodes[j][7]);
+  //       // Serial.print("Noeud de depart ");
+  //       // Serial.print(nodes[j][0]);
+  //       // Serial.print(" : ");
+  //       // Serial.print(nodes[j][1]);
+  //       // Serial.print(" ");
+  //       // Serial.print(nodes[j][2]);
+  //       // Serial.print(" ");
+  //       // Serial.print(nodes[j][3]);
+  //       // Serial.print(" ");
+  //       // Serial.print(nodes[j][4]);
+  //       // Serial.print(" ");
+  //       // Serial.print(nodes[j][5]);
+  //       // Serial.print(" ");
+  //       // Serial.print(nodes[j][6]);
+  //       // Serial.print(" ");
+  //       // Serial.println(nodes[j][7]);
       }
       if (nodes[j][3] == 2) { // Noeud d'arrivée
         goalNode[0] = nodes[j][0];
@@ -170,9 +186,9 @@ void loop() {
   //   // Appeler la fonction A* et afficher les résultats
   //   //int** chemin = aStar(matriceDecode, startNode, goalNode);
   //   // Reconstruire et afficher le chemin parcouru
-
-    int* cheminReconstruit = path( dijkstra(nodes, startNode), startNode, goalNode);
     int* dijkstraph = dijkstra(nodes, startNode);
+    int* cheminReconstruit = antecedant( dijkstraph, goalNode, startNode);
+    
     Serial.println("Liste d'antecedants :");
     for (int j = 0; j < 36; j++) {
       Serial.print(dijkstraph[j]);
@@ -184,16 +200,17 @@ void loop() {
       Serial.print(" ");
     }
 
+
     // Libérer la mémoire allouée pour le chemin reconstruit
     free(cheminReconstruit);
 
     // Libérer la mémoire allouée pour le chemin
     //free(chemin);
     free(nodes); // Libérer la mémoire allouée
-    free(matriceDecode); // Libérer la mémoire allouée
     i++;
-    //viewColor();
+  //   //viewColor();
   }
+
   
   // Petit délai pour éviter une utilisation excessive du CPU
   delay(DELAI_BOUCLE_MS);
