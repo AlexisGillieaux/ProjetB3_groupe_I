1ff9dcac5ea4c84077e147b59cbce2789622bbbf
AlexisGillieaux <etu55085@student.henallux.be>
2025-05-21 12:29:43 +0200

    Add initial state handling and transition functions; implement LED control and setup for RFID and IR management

---
+++ Modele-Projet-B3-main/include/etatSequence.h
@@ -0,0 +1,12 @@
+#ifndef ETAT_INITIAL_H
+#define ETAT_INITIAL_H
+
+#include "config.h"
+
+// Déclaration des fonctions
+void EtatInitial();
+bool transition_Initial_Attente();
+bool transition_Initial_Decodage();
+bool transition_Initial_MainDroiteFauteuil();
+
+#endif
